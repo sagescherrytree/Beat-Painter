4f366c71b8bcca1bfe184a2c3c2654555587deb2
Rachel Lin <raclin555@gmail.com>
2024-04-17 16:52:48 -0400

    home screen and song select UI

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    public void GoToScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitApp() {
+        Application.Quit();
+        Debug.Log("Application has quit");
+    }
+}
