b249dc7e7a10f0d464efb557e99cc2a4496b05fa
Jackie Li <aurorali99@gmail.com>
2024-04-19 13:48:28 -0400

    WIP: Connecting canvas to saber logic.
    But the print statements are printing.... T_T.

--- Assets/Scripts/Saber.cs
+++ Assets/Scripts/Saber.cs
@@ -7,11 +7,22 @@ public class Saber : MonoBehaviour
     // I do not know what I am doing XD.
     public LayerMask layer;
     private Vector3 previousPosition;
+    // Try something.
+    public GameObject canvas;
+    private MeshRenderer canvasRenderer;
+    private Material[] canvasMats;
+    private int counter;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        counter = 0;
+        canvasRenderer = canvas.GetComponent<MeshRenderer>();
+        if (canvasRenderer == null)
+        {
+            Debug.Log("Error: Canvas MeshRenderer does not exist.");
+        }
+        canvasMats = canvasRenderer.materials;
     }
 
     // Update is called once per frame
@@ -23,9 +34,19 @@ public class Saber : MonoBehaviour
             if (Vector3.Angle(transform.position - previousPosition, hit.transform.up) > 130)
             {
                 Destroy(hit.transform.gameObject);
+                ChangeMaterial(counter++);
                 Debug.Log("Found cube!");
             }
         }
         previousPosition = transform.position;
+    }
+
+    void ChangeMaterial(int counter)
+    {
+        if (counter < canvasMats.Length)
+        {
+            canvasMats[counter].color = Color.white;
+            Debug.Log("Colour change.");
+        }
     }
 }
