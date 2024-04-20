f434e69f9a737ad045a8b2bf0fde36c627b56ebe
Jackie Li <aurorali99@gmail.com>
2024-04-19 23:18:50 -0400

    Materials now disappear in an order!

--- Assets/Scripts/Saber.cs
+++ Assets/Scripts/Saber.cs
@@ -12,6 +12,7 @@ public class Saber : MonoBehaviour
     private MeshRenderer canvasRenderer;
     private Material[] canvasMats;
     private int counter;
+    private bool isMatVisible = true;
 
     // Start is called before the first frame update
     void Start()
@@ -43,9 +44,17 @@ public class Saber : MonoBehaviour
 
     void ChangeMaterial(int counter)
     {
+        Material mat = canvasMats[counter];
+        isMatVisible = !isMatVisible;
         if (counter < canvasMats.Length)
         {
-            canvasMats[counter].color = Color.white;
+            mat.SetInt("_SrcBlend", isMatVisible ? (int)UnityEngine.Rendering.BlendMode.One : (int)UnityEngine.Rendering.BlendMode.Zero);
+            mat.SetInt("_DstBlend", isMatVisible ? (int)UnityEngine.Rendering.BlendMode.Zero : (int)UnityEngine.Rendering.BlendMode.One);
+            mat.SetInt("_ZWrite", isMatVisible ? 1 : 0);
+            mat.DisableKeyword("_ALPHATEST_ON");
+            mat.DisableKeyword("_ALPHABLEND_ON");
+            mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            mat.renderQueue = isMatVisible ? -1 : 3000;
             Debug.Log("Colour change.");
         }
     }
