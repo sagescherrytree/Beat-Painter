587da6b00d5c9bbfe81c966acb63e98b11693553
Rachel Lin <raclin555@gmail.com>
2024-05-01 02:17:12 -0400

    added simple score system

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,8 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class UI : MonoBehaviour
 {
+    
+    
+    [SerializeField] public GameObject LossScreen;
+    [SerializeField] public GameObject WinScreen;
+
+    public static UI manager;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highscoreText;
+    public int score;
+
+    private void Awake() {
+        manager = this;
+    }
+
     public void GoToScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
     }
@@ -14,5 +29,22 @@ public class UI : MonoBehaviour
     public void QuitApp() {
         Application.Quit();
         Debug.Log("Application has quit");
+    }
+
+    public void LoseLevel() {
+        LossScreen.SetActive(true);
+    }
+
+    public void WinLevel() {
+        WinScreen.SetActive(true);
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void SetScore(int amnt) {
+        score = amnt;
+    }
+
+    public void IncreaseScore(int amnt) {
+        score += amnt;
     }
 }
