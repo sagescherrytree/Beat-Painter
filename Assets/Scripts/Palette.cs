e6624e267dc29605de610ed93738706264e0652f
Rachel Lin <raclin555@gmail.com>
2024-05-05 15:02:01 -0400

    changed palette script to update emission to selected color

--- Assets/Scripts/Palette.cs
+++ Assets/Scripts/Palette.cs
@@ -7,6 +7,15 @@ public class Palette : MonoBehaviour
     public int currColor;
     public int maxColor;
 
+    public Material col1;
+    public Material col2;
+    public Material col3;
+    public Material col4;
+    public Material col5;
+    public Material col6;
+    public Material col7;
+    public Material col8;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +38,49 @@ public class Palette : MonoBehaviour
             if (currColor < 1) {
                 currColor = maxColor;
             }
+        }
+
+        switch (currColor) {
+            case 1:
+                col2.DisableKeyword("_EMISSION");
+                col8.DisableKeyword("_EMISSION");
+                col1.EnableKeyword("_EMISSION");
+                break;
+            case 2:
+                col3.DisableKeyword("_EMISSION");
+                col1.DisableKeyword("_EMISSION");
+                col2.EnableKeyword("_EMISSION");
+                break;
+            case 3:
+                col4.DisableKeyword("_EMISSION");
+                col2.DisableKeyword("_EMISSION");
+                col3.EnableKeyword("_EMISSION");
+                break;
+            case 4:
+                col5.DisableKeyword("_EMISSION");
+                col3.DisableKeyword("_EMISSION");
+                col4.EnableKeyword("_EMISSION");
+                break;
+            case 5:
+                col6.DisableKeyword("_EMISSION");
+                col4.DisableKeyword("_EMISSION");
+                col5.EnableKeyword("_EMISSION");
+                break;
+            case 6:
+                col7.DisableKeyword("_EMISSION");
+                col5.DisableKeyword("_EMISSION");
+                col6.EnableKeyword("_EMISSION");
+                break;
+            case 7:
+                col8.DisableKeyword("_EMISSION");
+                col6.DisableKeyword("_EMISSION");
+                col7.EnableKeyword("_EMISSION");
+                break;
+            case 8:
+                col1.DisableKeyword("_EMISSION");
+                col7.DisableKeyword("_EMISSION");
+                col8.EnableKeyword("_EMISSION");
+                break;
         }
         Invoke("DelayedAction", 1.0f);
     }
