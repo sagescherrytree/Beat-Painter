8b6f272c408b92dd9bb411f1675bd22cf62c4943
sagescherrytree <aurorali99@gmail.com>
2024-05-05 16:56:06 -0400

    brush tip color updates

--- Assets/Scripts/Palette.cs
+++ Assets/Scripts/Palette.cs
@@ -51,49 +51,49 @@ public class Palette : MonoBehaviour
                     col2.DisableKeyword("_EMISSION");
                     col8.DisableKeyword("_EMISSION");
                     col1.EnableKeyword("_EMISSION");
-                    mats[0] = col1;
+                    mats[3] = col1;
                     break;
                 case 2:
                     col3.DisableKeyword("_EMISSION");
                     col1.DisableKeyword("_EMISSION");
                     col2.EnableKeyword("_EMISSION");
-                    mats[0] = col2;
+                    mats[3] = col2;
                     break;
                 case 3:
                     col4.DisableKeyword("_EMISSION");
                     col2.DisableKeyword("_EMISSION");
                     col3.EnableKeyword("_EMISSION");
-                    mats[0] = col3;
+                    mats[3] = col3;
                     break;
                 case 4:
                     col5.DisableKeyword("_EMISSION");
                     col3.DisableKeyword("_EMISSION");
                     col4.EnableKeyword("_EMISSION");
-                    mats[0] = col4;
+                    mats[3] = col4;
                     break;
                 case 5:
                     col6.DisableKeyword("_EMISSION");
                     col4.DisableKeyword("_EMISSION");
                     col5.EnableKeyword("_EMISSION");
-                    mats[0] = col5;
+                    mats[3] = col5;
                     break;
                 case 6:
                     col7.DisableKeyword("_EMISSION");
                     col5.DisableKeyword("_EMISSION");
                     col6.EnableKeyword("_EMISSION");
-                    mats[0] = col6;
+                    mats[3] = col6;
                     break;
                 case 7:
                     col8.DisableKeyword("_EMISSION");
                     col6.DisableKeyword("_EMISSION");
                     col7.EnableKeyword("_EMISSION");
-                    mats[0] = col7;
+                    mats[3] = col7;
                     break;
                 case 8:
                     col1.DisableKeyword("_EMISSION");
                     col7.DisableKeyword("_EMISSION");
                     col8.EnableKeyword("_EMISSION");
-                    mats[0] = col8;
+                    mats[3] = col8;
                     break;
             }
             paintbrush.materials = mats;
