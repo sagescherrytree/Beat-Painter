e41531bad345663de043a0dce933a83226337c3a
Rachel Lin <raclin555@gmail.com>
2024-05-07 15:22:27 -0400

    changed radial palette mesh

---
+++ Assets/Scripts/PaletteMerge.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaletteMerge : MonoBehaviour
+{
+    public int currColor;
+    public float test;
+
+    public Material[] colors;
+
+    public MeshRenderer paintbrush;
+    public ParticleSystemRenderer particles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currColor = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int direction = 0;
+            if (OVRInput.GetDown(OVRInput.Button.Three))
+            {
+                direction += 1;
+            }
+            if (OVRInput.GetDown(OVRInput.Button.Four))
+            {
+                direction -= 1;
+            }
+
+            if (direction != 0)
+            {
+                colors[currColor].DisableKeyword("_EMISSION");
+                currColor = (currColor + direction + colors.Length) % colors.Length;
+                Material currMat = colors[currColor];
+                currMat.EnableKeyword("_EMISSION");
+                Material[] bMats = paintbrush.materials;
+                Material[] pMats = particles.materials;
+                bMats[3] = currMat;
+                pMats[0] = currMat;
+                paintbrush.materials = bMats;
+                particles.materials = pMats;
+            }
+    }
+}
